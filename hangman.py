91cc03665189fd236b63b4177e890994b140f4f1
Almir2021 <bosnjicalmir@gmail.com>
2023-09-17 00:37:40 +0200

    almost there on right path

--- hangman.py
+++ hangman.py
@@ -16,22 +16,29 @@ for l in range(lenght_of_word):
     guessing_word.append(" ")
     # print(chosen_list[l])
 # TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-if lifes <= 7:
-    for i in range(7):
+
+while lifes <= 7:
         guess = str(input("Take a guess  "))
         guess = guess.lower()
         print(guess)
     # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-        for l in range(lenght_of_word):
+     
+        if guess in chosen_word:
+           for l in range(lenght_of_word):
             # print(chosen_list[l])
-            if guess == chosen_list[l]:
+             if guess == chosen_list[l]:
                 guessing_word.pop(l)
                 guessing_word.insert(l, guess)
-            else:
-                lifes += 1
+        else:
+              lifes += 1
 
         print(guessing_word)
-else:
+
+        if all([char in guessing_word for char in chosen_list]):
+         print("You won") 
+         break
+
+if lifes >=7:
     print("You lost")
 
-print(lifes)
+
