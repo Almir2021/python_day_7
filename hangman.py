b644f52ee954bb322ae877eaacead9a6d9fa904b
Almir2021 <bosnjicalmir@gmail.com>
2023-09-19 12:14:49 +0200

    workig as expected

--- hangman.py
+++ hangman.py
@@ -6,8 +6,8 @@ import random
 import hangman_art
 hangman_art.stages 
 
-
-word_list = ["aardvark", "baboon", "camel"]
+import hangman_words 
+word_list = hangman_words.word_list
 
 # TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 chosen_word = random.choice(word_list)
@@ -17,6 +17,7 @@ dispaly = 0
 already_guessed = []
 lenght_of_word = len(chosen_word)
 chosen_list = list(chosen_word)
+print(hangman_art.logo)
 for l in range(lenght_of_word):
     print("_ ", end="")
 print()
@@ -30,16 +31,17 @@ while lifes != 0:
         guess = guess.lower()
         print(guess)
     # TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-     
+        if guess in guessing_word:
+           print(f" Letter {guess} you already guessed")
+
         if guess in chosen_word:
            for l in range(lenght_of_word):
             # print(chosen_list[l])
              if guess == chosen_list[l]:
                 guessing_word.pop(l)
                 guessing_word.insert(l, guess)
-                already_guessed.append(guess)
-                for letter in already_guessed:
-                    if letter == 
+                 
+                
         else:
               lifes -= 1
               if lifes <=5  :
@@ -55,5 +57,6 @@ while lifes != 0:
         
         if lifes == 0:
             print("You lost")
+            print(f"the word to guess was {chosen_word} ")
 
 
